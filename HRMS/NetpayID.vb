0fe6863d29aa621766f0e0ea14feec9e8f00818a
jiaweiloo <loojiawei97@gmail.com>
2018-04-15 02:37:46 +0800

    Added netpay id module by Jia Wei

---
+++ HRMS/NetpayID.vb
@@ -0,0 +1,15 @@
+﻿Module NetpayID
+    Public initial As Integer = 1
+
+    Public Function GetNextId() As String
+        Dim db As New HRMS_DBLinq2DataContext
+        Dim netpayList = (From c In db.netpays).ToList
+        Dim ntpay As netpay
+        Try
+            ntpay = netpayList.ElementAt(netpayList.Count - 1)
+            Return (ntpay.payroll_id + 1).ToString("00000")
+        Catch ex As Exception
+            Return (initial).ToString("30000")
+        End Try
+    End Function
+End Module
