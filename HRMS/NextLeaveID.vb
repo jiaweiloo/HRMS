763aff8cb70710283f28e2dd503e5c77be6b840a
JJzaii <JJzaii@JJZAII96>
2018-04-14 14:26:46 +0800

    JJ

---
+++ HRMS/NextLeaveID.vb
@@ -0,0 +1,25 @@
+﻿Module NextLeaveID
+    Public Leave(99999) As Leave
+    Public Count As Integer = 0
+
+    Public Function GetNextId() As String
+        Count = 0
+        Dim db As New DataClasses1DataContext
+        Dim lastid As String
+        Dim id As String
+        id = "L0000"
+        For Each a In db.Leaves
+            lastid = a.leave_id
+            Count = Count + 1
+
+        Next
+
+        Try
+            'Return id & (Count).ToString()
+            Return (Count).ToString("13WAR00000")
+        Catch ex As Exception
+            Return ex.Message
+        End Try
+
+    End Function
+End Module
