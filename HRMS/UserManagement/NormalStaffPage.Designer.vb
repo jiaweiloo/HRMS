fb93b47707d00cfbbb81ad33e2a92db8a665ad7b
JJzaii <JJzaii@JJZAII96>
2018-04-15 00:24:34 +0800

    JJ 1234

--- HRMS/UserManagement/NormalStaffPage.Designer.vb
+++ HRMS/UserManagement/NormalStaffPage.Designer.vb
@@ -24,12 +24,14 @@ Partial Class NormalStaffPage
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnApplyLeave = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(280, 22)
+        Me.Label1.Location = New System.Drawing.Point(306, 19)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(51, 17)
         Me.Label1.TabIndex = 0
@@ -37,24 +39,37 @@ Partial Class NormalStaffPage
         '
         'btnApplyLeave
         '
-        Me.btnApplyLeave.Location = New System.Drawing.Point(261, 144)
+        Me.btnApplyLeave.Location = New System.Drawing.Point(531, 144)
         Me.btnApplyLeave.Name = "btnApplyLeave"
-        Me.btnApplyLeave.Size = New System.Drawing.Size(85, 45)
+        Me.btnApplyLeave.Size = New System.Drawing.Size(85, 77)
         Me.btnApplyLeave.TabIndex = 1
         Me.btnApplyLeave.Text = "&Apply Leave"
         Me.btnApplyLeave.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.HRMS.My.Resources.Resources.holiday_mood
+        Me.PictureBox1.Location = New System.Drawing.Point(30, 71)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(412, 227)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
         '
         'NormalStaffPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(711, 360)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.btnApplyLeave)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "NormalStaffPage"
         Me.Text = "NormalStaffPage"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -62,4 +77,5 @@ Partial Class NormalStaffPage
 
     Friend WithEvents Label1 As Label
     Friend WithEvents btnApplyLeave As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
