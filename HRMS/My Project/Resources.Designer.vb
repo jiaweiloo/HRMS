258b05f18d75953433a0fd8ed742221bf1f7d1ad
jiaweiloo <loojiawei97@gmail.com>
2018-04-15 17:00:18 +0800

    Added picture

--- HRMS/My Project/Resources.Designer.vb
+++ HRMS/My Project/Resources.Designer.vb
@@ -153,6 +153,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property MyPayrollHistory() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("MyPayrollHistory", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property password() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("password", resourceCulture)
